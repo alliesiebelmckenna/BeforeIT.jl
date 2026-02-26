c78c1eb88b73ea8ca106115f5871e622394faa74
Allie Siebel-McKenna <allie.siebel-mckenna@gov.bc.ca>
2026-02-26 10:49:55 -0800

    Amend path to config file

--- src/utils/standard_params_initial_conditions.jl
+++ src/utils/standard_params_initial_conditions.jl
@@ -1,13 +1,21 @@
-using JLD2
+using JLD2, YAML
 
 struct InitialState
     parameters::Dict{String, Any}
     initial_conditions::Dict{String, Any}
 end
 
+paras = YAML.load_file("bcfin/core-test/config.yaml")
+
 dir = joinpath(splitpath(dirname(pathof(@__MODULE__)))[1:(end - 1)])
 
 parameters = load(joinpath(dir, "data/austria/parameters/2010Q1.jld2"))
+
+parameters["vec_tau_PIT"] = paras["base_vec_tau_PIT"]
+parameters["vec_thr_lo_PIT"] = paras["base_vec_tau_PIT"]
+parameters["vec_thr_up_PIT"] = paras["base_vec_tau_PIT"]
+parameters["vec_addition_PIT"] = paras["base_vec_tau_PIT"]
+
 initial_conditions = load(joinpath(dir, "data/austria/initial_conditions/2010Q1.jld2"))
 
 const AUSTRIA2010Q1 = InitialState(parameters, initial_conditions)
