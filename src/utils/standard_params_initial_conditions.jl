173cec2835063a24c003012c61bfe4a146c0ef1e
Allie Siebel-McKenna <allie.siebel-mckenna@gov.bc.ca>
2026-02-26 11:01:52 -0800

    Amend parameter definitions

--- src/utils/standard_params_initial_conditions.jl
+++ src/utils/standard_params_initial_conditions.jl
@@ -5,16 +5,23 @@ struct InitialState
     initial_conditions::Dict{String, Any}
 end
 
+# Load user-defined parameters
 paras = YAML.load_file("bcfin/core-test/config.yaml")
 
 dir = joinpath(splitpath(dirname(pathof(@__MODULE__)))[1:(end - 1)])
 
 parameters = load(joinpath(dir, "data/austria/parameters/2010Q1.jld2"))
 
-parameters["vec_tau_PIT"] = paras["base_vec_tau_PIT"]
-parameters["vec_thr_lo_PIT"] = paras["base_vec_tau_PIT"]
-parameters["vec_thr_up_PIT"] = paras["base_vec_tau_PIT"]
-parameters["vec_addition_PIT"] = paras["base_vec_tau_PIT"]
+# Extract variables for easy access
+vec_tau_PIT = paras["base_vec_tau_PIT"]
+vec_tau_thr_lo = paras["base_vec_thr_lo_PIT"]
+
+# Add to existing parameters 
+parameters["vec_tau_PIT"] = vec_tau_PIT
+parameters["vec_thr_lo_PIT"] = vec_tau_thr_lo
+parameters["vec_thr_up_PIT"] = upper_thresholds(vec_tau_PIT)
+parameters["vec_addition_PIT"] = addition(vec_tau_PIT, vec_tau_thr_lo)
+# NOTE: this only changes parameters for AUSTRIA2010Q1
 
 initial_conditions = load(joinpath(dir, "data/austria/initial_conditions/2010Q1.jld2"))
 
