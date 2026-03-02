37f185dac51280b25c6d7913182883fa90fa1224
Allie Siebel-McKenna <allie.siebel-mckenna@gov.bc.ca>
2026-03-02 10:16:29 -0800

    Add new federal income tax parameters to model properties

--- src/model_init/init_properties.jl
+++ src/model_init/init_properties.jl
@@ -44,6 +44,11 @@ Bit.@object mutable struct Properties(Object) <: AbstractProperties
     vec_thr_lo_PIT::Vector{Bit.typeFloat}
     vec_thr_up_PIT::Vector{Bit.typeFloat}
     vec_addition_PIT::Vector{Bit.typeFloat}
+    # New federal income tax parameters
+    vec_tau_fed_PIT::Vector{Bit.typeFloat}
+    vec_thr_lo_fed_PIT::Vector{Bit.typeFloat}
+    vec_thr_up_fed_PIT::Vector{Bit.typeFloat}
+    vec_addition_fed_PIT::Vector{Bit.typeFloat}
 end
 
 function Properties(parameters::Dict{String, Any}, initial_conditions)
@@ -98,16 +103,23 @@ function Properties(parameters::Dict{String, Any}, initial_conditions)
     E_k = typeFloat(initial_conditions["E_k"])
     r_bar = typeFloat(initial_conditions["r_bar"])
 
-    # personal income tax related parameters
+    # provincial personal income tax related parameters
     vec_tau_PIT = Vector{typeFloat}(vec(parameters["vec_tau_PIT"]))
     vec_thr_lo_PIT = Vector{typeFloat}(vec(parameters["vec_thr_lo_PIT"]))
     vec_thr_up_PIT = Vector{typeFloat}(vec(parameters["vec_thr_up_PIT"]))
     vec_addition_PIT = Vector{typeFloat}(vec(parameters["vec_addition_PIT"]))
 
+    # federal personal income tax related parameters
+    vec_tau_fed_PIT = Vector{typeFloat}(vec(parameters["vec_tau_fed_PIT"]))
+    vec_thr_lo_fed_PIT = Vector{typeFloat}(vec(parameters["vec_thr_lo_fed_PIT"]))
+    vec_thr_up_fed_PIT = Vector{typeFloat}(vec(parameters["vec_thr_up_fed_PIT"]))
+    vec_addition_fed_PIT = Vector{typeFloat}(vec(parameters["vec_addition_fed_PIT"]))
+
     return Properties(
         G, T_prime, H_act, H_inact, J, L, I_s, I, H, tau_INC, tau_FIRM, tau_VAT, tau_SIF,
         tau_SIW, tau_EXPORT, tau_CF, tau_G, theta_UB, psi, psi_H, mu, theta_DIV, theta, zeta, zeta_LTV,
         zeta_b, b_CF_g, b_CFH_g, b_HH_g, c_G_g, c_E_g, c_I_g, a_sg, C, D_H, K_H, sb_other, E_k, r_bar,
-        vec_tau_PIT, vec_thr_lo_PIT, vec_thr_up_PIT, vec_addition_PIT
+        vec_tau_PIT, vec_thr_lo_PIT, vec_thr_up_PIT, vec_addition_PIT,
+        vec_tau_fed_PIT, vec_thr_lo_fed_PIT, vec_thr_up_fed_PIT, vec_addition_fed_PIT
     )
 end
